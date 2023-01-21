cb977ea19a3932c2d115f13eb29cd2d43c79448f
Prakash Joshi <prakash.joshi167@hotmail.com>
2023-01-21 18:24:31 +0530

    navbar props added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NavBar from './components/NavBar';
 function App() {
   return (
    <>
+   <NavBar title="Text Utils" home="Home" about="About-Us"/>
    <NavBar />
    </>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,23 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export default function NavBar() {
+
+export default function NavBar(props) {
     return (
         <>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/">Text Utils</a>
+                    <a className="navbar-brand" href="/">{props.title}</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="/">Home</a>
+                                <a className="nav-link active" aria-current="page" href="/">{props.home}</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/">About Us</a>
+                                <a className="nav-link" href="/">{props.about}</a>
                             </li>
                         </ul>
                         <form className="d-flex" role="search">
@@ -27,4 +29,17 @@ export default function NavBar() {
             </nav>
         </>
     )
+}
+
+
+NavBar.prototype = {
+    title : PropTypes.string.isRequired,
+    home : PropTypes.string.isRequired,
+    about : PropTypes.string.isRequired
+}
+
+NavBar.defaultProps = {
+    title : "Title",
+    home : "home test",
+    about : "about text"
 }

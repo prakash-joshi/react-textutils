cec01889b645e6a356372d5820194d6c163f2b9e
Prakash Joshi <prakash.joshi167@hotmail.com>
2023-01-24 21:29:49 +0530

    some fixes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
+import About from './components/About';
+import Alert from './components/Alert';
+import TextForm from './components/TextForm';
 import React, { useState } from 'react';
 
 function App() {
 
   const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
   const showAlert =()=>{
     
   }
@@ -19,9 +24,15 @@ function App() {
       document.body.style.backgroundColor = 'white'
     }
   }
+
   return (
     <>
       <NavBar title="Text Utils" home="Home" about="About-Us" mode={mode} toggleMode={toggleMode} />
+      {/* <About /> */}
+      <Alert alertMessage="Holy guacamole! You should check in on some of those fields below." />
+      <div className="container my-3 mx-3">
+        <TextForm heading="Enter the text to analyze below." />
+      </div>
     </>
   );
 }
